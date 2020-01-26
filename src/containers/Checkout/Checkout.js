0b0ce500930f170f4cd1ab05a54afc597d686fa0
lucas <lucasjwilber@gmail.com>
2020-01-25 20:14:36 -0800

    added reducer for orders, added redirect for contact-data page to prevent an error

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,35 +1,11 @@
 import React, {Component} from "react";
-import { Route } from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import ContactData from '../../containers/Checkout/ContactData/ContactData.js';
 import CheckoutSummary from "../../components/Order/CheckoutSummary.js";
 
 class Checkout extends Component {
-  // state = {
-  //   ingredients: {
-  //     salad: 0,
-  //     cheese: 0,
-  //     bacon: 0,
-  //     meat: 0
-  //   },
-  //   price: 4,
-  // };
-
-  // componentDidMount() {
-  //   const query = new URLSearchParams(this.props.location.search);
-  //   const ingredients = {};
-  //   let price = 0;
-  //   for (let param of query.entries()) {
-  //     if (param[0] === 'price') {
-  //       price = +param[1];
-  //     } else {
-  //       ingredients[param[0]] = +param[1];
-  //     }
-  //   }
-  //   this.setState({ingredients: ingredients, price: price})
-  // }
-
 
   proceedToCheckout = () => {
     console.log('proceed to checkout clicked');
@@ -42,30 +18,30 @@ class Checkout extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary 
-          ingredients={this.props.ingrs}
-          proceed={this.proceedToCheckout}
-          cancel={this.cancelCheckout}
-          price={this.props.price} />
-          
+    //without this redirect, when the page is refreshed during checkout the app crashes due to undefined state
+    let summary = <Redirect to="/" />;
+    if (this.props.ingrs) {
+      summary = (
+        <div>
+          <CheckoutSummary 
+            ingredients={this.props.ingrs}
+            proceed={this.proceedToCheckout}
+            cancel={this.cancelCheckout}
+            price={this.props.price} />
           <Route 
             path={`${this.props.match.path}/contact-info`}
-            // props are passed on so that ContactData inherits router props
-            //(this workaround isn't needed when using redux to pass the props around)
-            // render={(props) => <ContactData ingredients={this.state.ingrs} price={this.state.price} {...props} />} 
-            component={ContactData}
-            />
-      </div>
-    );
+            component={ContactData} />
+        </div>
+      );
+    }
+    return summary;
   }
 }
 
 const mapStateToProps = state => {
   return {
-    ingrs: state.ingredients,
-    price: state.totalPrice,
+    ingrs: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
   }
 }
 
